7556b6ca10ad21efbf60094ee55070b38c77645e
Mikael Gyth <gythmikael@gmail.com>
2018-02-12 07:44:53 +0100

    Initial

---
+++ AreaPacker/AreaPackerToolWindow.cs
@@ -0,0 +1,34 @@
+﻿namespace AreaPacker
+{
+    using System;
+    using System.Runtime.InteropServices;
+    using Microsoft.VisualStudio.Shell;
+
+    /// <summary>
+    /// This class implements the tool window exposed by this package and hosts a user control.
+    /// </summary>
+    /// <remarks>
+    /// In Visual Studio tool windows are composed of a frame (implemented by the shell) and a pane,
+    /// usually implemented by the package implementer.
+    /// <para>
+    /// This class derives from the ToolWindowPane class provided from the MPF in order to use its
+    /// implementation of the IVsUIElementPane interface.
+    /// </para>
+    /// </remarks>
+    [Guid("f0b2973c-d5cb-401c-8b6f-c46fd07fbeb6")]
+    public class AreaPackerToolWindow : ToolWindowPane
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AreaPackerToolWindow"/> class.
+        /// </summary>
+        public AreaPackerToolWindow() : base(null)
+        {
+            this.Caption = "AreaPackerToolWindow";
+
+            // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
+            // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
+            // the object returned by the Content property.
+            this.Content = new AreaPackerToolWindowControl();
+        }
+    }
+}
